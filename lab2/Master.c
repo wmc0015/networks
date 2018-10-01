dc78078ad79a4b67f7d60e8f7b26eb34d49568d3
Jack Mullins <jackmullins@auburn.edu>
2018-10-01 14:48:10 -0500

    Started work on lab2/Master.c

--- lab2/Master.c
+++ lab2/Master.c
@@ -12,6 +12,18 @@
 #include <signal.h>
 
 #define BACKLOG 10
+#define MAX_IP_SIZE 15
+#define MAX_DATA_SIZE 100
+
+struct request {
+	char groupID;
+	int magicNum;
+	char timeToLive;
+	char ridDest;
+	char ridSource;
+	char[64] message;
+	char checksum;
+};
 
 void sigchld_handler(int s)
 {
@@ -34,5 +46,116 @@ void *get_in_addr(struct sockaddr *sa)
 }
 
 int main(void) {
+	// SOCKET VARIABLES
+	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_storage their_addr; // connector's address information
+	socklen_t sin_size;
+	struct sigaction sa;
+	int yes=1;
+	char s[INET6_ADDRSTRLEN];
+	int rv;
+	int portAsInt;
+	char port[5];
+
+	// LAB VARIABLES
+	int nextRID;
+	char[MAX_IP_SIZE] nextSlaveIP;
+	int nextSlaveID;
+	struct request requestToJoin;
 	
+	if (argc != 2) {
+		// report missing port
+		printf("missing port\n");
+		exit(1);
+	}
+
+	strcpy(port, argv[1]);
+	portAsInt = strtol(port, NULL, 0);
+	if ((portAsInt < 1024) || (portAsInt > 65535)) {
+		// throw port out of bounds error
+		printf("port out of bounds\n");
+		exit(1);
+	}
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+	
+	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+	
+	// loop through all the results and bind to the first we can
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("master: socket");
+			continue;
+		}
+
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+				sizeof(int)) == -1) {
+			perror("setsockopt");
+			exit(1);
+		}
+
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("master: bind");
+			continue;
+		}
+
+		break;
+	}
+
+	freeaddrinfo(servinfo);
+
+	if (p == NULL)  {
+		fprintf(stderr, "master: failed to bind\n");
+		exit(1);
+	}
+
+	if (listen(sockfd, BACKLOG) == -1) {
+		perror("listen");
+		exit(1);
+	}
+
+	sa.sa_handler = sigchld_handler; // reap all dead processes
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+		perror("sigaction");
+		exit(1);
+	}
+
+	printf("master: waiting for connections...\n");
+
+	// receive request
+
+	while(1) {  // main accept() loop
+		sin_size = sizeof their_addr;
+		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+		if (new_fd == -1) {
+			perror("accept");
+			continue;
+		}
+
+		inet_ntop(their_addr.ss_family,
+			get_in_addr((struct sockaddr *)&their_addr),
+			s, sizeof s);
+		printf("master: got connection from %s\n", s);
+
+		// if problems, fork
+		if ((numbytes = recv(sockfd, buf, MAX_DATA_SIZE-1, 0)) == -1) {
+	    	perror("recv");
+	   		exit(1);
+		}
+
+		buf[numbytes] = '\0';
+
+		//memset to convert char[] to struct request
+	}
 }
